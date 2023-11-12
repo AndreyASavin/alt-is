f438a44ba9dc818a0499475830cee84d623d333e
Andrey <savin.andrey199@gmail.com>
2023-11-12 20:30:45 +0300

    feat: add basic layout grid styles, header and footer

---
+++ alt-is-front/components/template/footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <div>Бизнесу</div>
+          <p>Корпоративным клиентам</p>
+          <p>Тендеры и госзакупки</p>
+          <p>Программы лояльности</p>
+        </div>
+        <div class="col-3">
+          <div>Покупателям</div>
+          <p>Магазины</p>
+          <p>Как оформить заказ</p>
+          <p>Доставка</p>
+          <p>Гарантия и возврат</p>
+          <p>Способы оплаты</p>
+          <p>Бонусная программа</p>
+          <p>Сервис</p>
+          <p>Справка по работе</p>
+        </div>
+        <div class="col-3">
+          <div>О компании</div>
+          <p>О нас</p>
+          <p>Реквизиты</p>
+          <p>Политика компании по сохранности персональных данных</p>
+          <p>Правила продажи на сайте</p>
+          <p>Производители</p>
+          <p>Вакансии</p>
+        </div>
+        <div class="col-3">
+          <div>г. Барнаул, ул. Малахова 94б</div>
+          <div>+7 (3852) 226-800</div>
+          <div>info@alt-is.ru</div>
+          <div>Наш Telegram</div>
+          <div>Политика конфиденциальности</div>
+        </div>
+      </div>
+      <hr/>
+      <div class="row">
+        <div class="col-12">
+          <div>Логотип</div>
+          <div>2013–2023 © ООО «Инженерные системы»</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ alt-is-front/components/template/header.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="header">
+    <div class="header__top">
+      <div class="header__top_container">
+        <div class="header__top_row">
+          <div class="header__top-menu">
+            <div class="top-menu__item">Информация</div>
+            <div class="top-menu__item">Условия</div>
+          </div>
+          <div class="header__top-navigation">
+            <div class="top-navigation__item">
+              г. Барнаул, ул. Малахова 94б
+            </div>
+            <div class="top-navigation__item">
+              Наш Telegram
+            </div>
+            <div class="top-navigation__item">
+              info@alt-is.ru
+            </div>
+            <div class="top-navigation__item">
+              +7&nbsp;(3852)&nbsp;226-800
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header__bottom">
+      <div class="header__bottom_container">
+        <div class="header__bottom_row">
+          <div class="header__catalog">
+            <div class="header__logo">
+              Логотип
+            </div>
+            <div class="header__catalog-btn">
+              Кнопка
+            </div>
+          </div>
+          <div class="header__menu">
+            <div class="menu__item">
+              Сравнение
+            </div>
+            <div class="menu__item">
+              Избранное
+            </div>
+            <div class="menu__item">
+              Заказы
+            </div>
+            <div class="menu__item">
+              Корзина
+            </div>
+            <div class="menu__item">
+              Войти
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
